0cabda5756a0d05e59d070d86388bf37bdaf7f59
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-06-08 21:33:30 +0000

    Move mocked files to test dir

--- cmake/ceedling.cmake
+++ cmake/ceedling.cmake
@@ -1,3 +1,5 @@
+include_guard(GLOBAL)
+
 option(CEEDLING_ENABLE_GCOV "Enable coverage" OFF)
 option(CEEDLING_ENABLE_SANITIZER "Enable sanitizer" OFF)
 option(CEEDLING_SANITIZER_DEFAULT "Enable sanitizer by default" ON)
@@ -24,17 +26,38 @@ function(add_unit_test)
     endif()
 
     add_executable(${UT_NAME} ${UT_UNIT_TEST})
-    target_include_directories(${UT_NAME} PRIVATE ${CMOCK_MOCK_DIR})
     target_link_libraries(${UT_NAME} PRIVATE ${UT_TARGET} cmock unity)
+
+    set(TEST_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${UT_NAME}.dir)
+    file(MAKE_DIRECTORY ${TEST_BINARY_DIR})
 
     unset(RUNNER_SOURCE)
     generate_runner(${UT_UNIT_TEST} RUNNER_SOURCE)
-    target_sources(${UT_NAME} PRIVATE ${RUNNER_SOURCE})
+    cmake_path(GET RUNNER_SOURCE STEM RUNNER_STEM)
+    set(TEST_RUNNER ${TEST_BINARY_DIR}/${RUNNER_STEM}.c)
+    add_custom_command(
+        OUTPUT ${TEST_RUNNER}
+        DEPENDS ${RUNNER_SOURCE}
+        COMMAND ${CMAKE_COMMAND} -E rename ${RUNNER_SOURCE} ${TEST_RUNNER}
+        COMMENT "Move ${RUNNER_STEM} to ${TEST_BINARY_DIR}"
+    )
+    target_sources(${UT_NAME} PRIVATE ${TEST_RUNNER})
+    target_include_directories(${UT_NAME} PRIVATE ${TEST_BINARY_DIR})
 
     foreach(HEADER IN LISTS UT_MOCK_HEADERS)
         unset(MOCK_SOURCE)
-        mock_header(${HEADER} MOCK_SOURCE)
-        target_sources(${UT_NAME} PRIVATE ${MOCK_SOURCE})
+        mock_header(${HEADER} MOCK_SOURCE MOCK_HEADER)
+        cmake_path(GET MOCK_SOURCE STEM MOCK_STEM)
+        set(TEST_MOCK_SOURCE ${TEST_BINARY_DIR}/${MOCK_STEM}.c)
+        set(TEST_MOCK_HEADER ${TEST_BINARY_DIR}/${MOCK_STEM}.h)
+        add_custom_command(
+            OUTPUT ${TEST_MOCK_SOURCE} ${TEST_MOCK_HEADER}
+            DEPENDS ${MOCK_SOURCE} ${MOCK_HEADER}
+            COMMAND ${CMAKE_COMMAND} -E rename ${MOCK_SOURCE} ${TEST_MOCK_SOURCE}
+            COMMAND ${CMAKE_COMMAND} -E rename ${MOCK_HEADER} ${TEST_MOCK_HEADER}
+            COMMENT "Move ${MOCK_STEM} to ${TEST_BINARY_DIR}"
+        )
+        target_sources(${UT_NAME} PRIVATE ${TEST_MOCK_SOURCE} )
     endforeach()
 
     if(CEEDLING_ENABLE_GCOV)
